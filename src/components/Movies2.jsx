1895094ac42966f742f15e0bb7cf7c5a326f4379
TadrosNasr <tadrosnasr7@gmail.com>
2025-03-12 16:23:05 +0200

    slider update

--- src/components/Movies2.jsx
+++ src/components/Movies2.jsx
@@ -13,18 +13,18 @@ const Movies2 = () => {
     return (
         <>
     {/* slider */}
-    <div id="carouselExampleAutoplaying" className="carousel slide mx-auto my-5 mt-5 " style={{ height:  "65vh" }} data-bs-ride="carousel">
+    <div id="carouselExampleAutoplaying" className="carousel slide mx-auto my-5 mt-5 " style={{ height:  "71vh" }} data-bs-ride="carousel">
         <div className="carousel-inner">
             <div className="carousel-item active"> {/* style={{ height: '90vh', width: '95vw'}} */}
-                <img src="https://pixel.disco.ott.showmax.com/uuid/04fa9049-3be1-3a44-8b8e-0e92750c90cf/COVER_TITLE_WIDE?language=en-US&proposition=SHOWMAX&version=400de697-7557-30fe-adf3-b18e9c27722c" style={{ height:  "70vh"}} className="d-block w-100 img-fluid" alt="Slide 3" />
+                <img src="https://pixel.disco.ott.showmax.com/uuid/04fa9049-3be1-3a44-8b8e-0e92750c90cf/COVER_TITLE_WIDE?language=en-US&proposition=SHOWMAX&version=400de697-7557-30fe-adf3-b18e9c27722c" style={{ height:  "75vh"}} className="d-block col-12 col-lg-9 mx-auto img-fluid" alt="Slide 3" />
             </div>
 
             <div className="carousel-item "> 
-                <img src="https://www.tallengestore.com/cdn/shop/products/Midway_2019_-_Hollywood_War_WW2_Original_Movie_Poster_41a0a72c-d984-4c8c-b1a4-a07243829752.jpg?v=1582782925"  style={{ height:  "70vh"}} className="d-block w-100 img-fluid" alt="Slide 1" />
+                <img src="https://www.tallengestore.com/cdn/shop/products/Midway_2019_-_Hollywood_War_WW2_Original_Movie_Poster_41a0a72c-d984-4c8c-b1a4-a07243829752.jpg?v=1582782925"  style={{ height:  "75vh"}} className="d-block col-12 col-lg-9 mx-auto img-fluid" alt="Slide 1" />
             </div>
             
             <div className="carousel-item">
-                <img src="https://blog.philo.com/wp-content/uploads/2023/09/John-Wick_Chapter-4.jpg" style={{ height:  "70vh"}} className="d-block w-100" alt="Slide 2" />
+                <img src="https://blog.philo.com/wp-content/uploads/2023/09/John-Wick_Chapter-4.jpg" style={{ height:  "75vh"}} className="d-block col-12 col-lg-9 mx-auto" alt="Slide 2" />
             </div>
             
         </div>
